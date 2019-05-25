c5200cc2435a486022b3c78fc3ab873819060ff8
Ash Zaki <ashleya.zaki@gmail.com>
2019-05-24 22:50:51 -0400

    setting up components

---
+++ personal-website/src/Containers/AshleyZaki.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+
+class AshleyZaki extends Component {
+
+   
+    render(){
+        return (
+            <Fragment>
+               
+                Hi
+               
+            </Fragment>
+        )
+    }
+}
+
+export default AshleyZaki;
---
+++ personal-website/src/Containers/Navbar.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+
+class Navbar extends Component {
+
+   
+    render(){
+        return (
+            <Fragment>
+               
+                
+               
+            </Fragment>
+        )
+    }
+}
+
+export default Navbar;
